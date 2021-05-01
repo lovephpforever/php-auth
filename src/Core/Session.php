b3e1f3d99e140379b181ecff229329b49060a636
lovephpforever <83395798+lovephpforever@users.noreply.github.com>
2021-05-01 04:26:01 -0400

    Add the stop method.

--- src/Core/Session.php
+++ src/Core/Session.php
@@ -78,6 +78,25 @@ final class Session
     }
 
     /**
+     * Stop a session and destroy all session data.
+     *
+     * @return bool Returns true on success or false on failure.
+     */
+    public function stop(): bool
+    {
+        $_SESSION = [];
+        if (\ini_get("session.use_cookies")) {
+            $params = \session_get_cookie_params();
+            \setcookie(\session_name(), '', \time() - 42000,
+                $params["path"], $params["domain"],
+                $params["secure"], $params["httponly"],
+                ['samesite' => $params["samesite"]]
+            );
+        }
+        return \session_destroy();
+    }
+
+    /**
      * Check to see if a session already exists.
      *
      * @return bool Returns true if one exists and false if not.
