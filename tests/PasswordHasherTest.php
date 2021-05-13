0eec7483d734c2d040fbce14446dce9b598336f9
lovephpforever <83395798+lovephpforever@users.noreply.github.com>
2021-05-13 02:29:11 -0400

    Add more tests.

--- tests/PasswordHasherTest.php
+++ tests/PasswordHasherTest.php
@@ -60,4 +60,189 @@ class PasswordHasherTest extends TestCase
         $hasher = new PasswordHasher(\PASSWORD_ARGON2ID);
         $this->assertEquals(\PASSWORD_ARGON2ID, $hasher->passwordAlgo);
     }
+
+    /**
+     * @return void Returns nothing.
+     */
+    public function testBcryptException(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $hasher = new PasswordHasher(\PASSWORD_BCRYPT);
+        $this->assertEquals(\PASSWORD_BCRYPT, $hasher->passwordAlgo);
+        $password = $hasher->compute('eP&=Mj]Vb],h$=Z@]LaK{b%V@M`(HY6"$Nd5UB]YjC^.BSuZM~J~LpY9nt\'8"Y{^bSA3{PqL(');
+    }
+
+    /**
+     * @return void Returns nothing.
+     */
+    public function testBcryptCompute(): void
+    {
+        $hasher = new PasswordHasher(\PASSWORD_BCRYPT);
+        $this->assertEquals(\PASSWORD_BCRYPT, $hasher->passwordAlgo);
+        $password = $hasher->compute('password');
+        $this->assertTrue(\is_string($password));
+    }
+
+    /**
+     * @return void Returns nothing.
+     */
+    public function testArgon2iCompute(): void
+    {
+        $hasher = new PasswordHasher(\PASSWORD_ARGON2I);
+        $this->assertEquals(\PASSWORD_ARGON2I, $hasher->passwordAlgo);
+        $password = $hasher->compute('password');
+        $this->assertTrue(\is_string($password));
+    }
+
+    /**
+     * @return void Returns nothing.
+     */
+    public function testArgon2idCompute(): void
+    {
+        $hasher = new PasswordHasher(\PASSWORD_ARGON2ID);
+        $this->assertEquals(\PASSWORD_ARGON2ID, $hasher->passwordAlgo);
+        $password = $hasher->compute('password');
+        $this->assertTrue(\is_string($password));
+    }
+
+    /**
+     * @return void Returns nothing.
+     */
+    public function testBcryptVerify(): void
+    {
+        $hasher = new PasswordHasher(\PASSWORD_BCRYPT);
+        $this->assertEquals(\PASSWORD_BCRYPT, $hasher->passwordAlgo);
+        $password = $hasher->compute('password');
+        $this->assertTrue(\is_string($password));
+        $this->assertTrue(!$hasher->verify('incorrect', $password));
+        $this->assertTrue($hasher->verify('password', $password));
+    }
+
+    /**
+     * @return void Returns nothing.
+     */
+    public function testArgon2iVerify(): void
+    {
+        $hasher = new PasswordHasher(\PASSWORD_ARGON2I);
+        $this->assertEquals(\PASSWORD_ARGON2I, $hasher->passwordAlgo);
+        $password = $hasher->compute('password');
+        $this->assertTrue(\is_string($password));
+        $this->assertTrue(!$hasher->verify('incorrect', $password));
+        $this->assertTrue($hasher->verify('password', $password));
+    }
+
+    /**
+     * @return void Returns nothing.
+     */
+    public function testArgon2idVerify(): void
+    {
+        $hasher = new PasswordHasher(\PASSWORD_ARGON2ID);
+        $this->assertEquals(\PASSWORD_ARGON2ID, $hasher->passwordAlgo);
+        $password = $hasher->compute('password');
+        $this->assertTrue(\is_string($password));
+        $this->assertTrue(!$hasher->verify('incorrect', $password));
+        $this->assertTrue($hasher->verify('password', $password));
+    }
+
+    /**
+     * @return void Returns nothing.
+     */
+    public function testBcrypt2Argon2iRehash(): void
+    {
+        $hasher = new PasswordHasher(\PASSWORD_BCRYPT);
+        $this->assertEquals(\PASSWORD_BCRYPT, $hasher->passwordAlgo);
+        $password = $hasher->compute('password');
+        $this->assertTrue(\is_string($password));
+        $hasher2 = new PasswordHasher(\PASSWORD_ARGON2I);
+        $this->assertEquals(\PASSWORD_ARGON2I, $hashe2->passwordAlgo);
+        $this->assertTrue($hashe2->needsRehash($password));
+        $password = $hasher->compute('password');
+        $this->assertTrue(\is_string($password));
+        $this->assertTrue(!$hashe2->needsRehash($password));
+    }
+
+    /**
+     * @return void Returns nothing.
+     */
+    public function testBcrypt2Argon2idRehash(): void
+    {
+        $hasher = new PasswordHasher(\PASSWORD_BCRYPT);
+        $this->assertEquals(\PASSWORD_BCRYPT, $hasher->passwordAlgo);
+        $password = $hasher->compute('password');
+        $this->assertTrue(\is_string($password));
+        $hasher2 = new PasswordHasher(\PASSWORD_ARGON2ID);
+        $this->assertEquals(\PASSWORD_ARGON2ID, $hashe2->passwordAlgo);
+        $this->assertTrue($hashe2->needsRehash($password));
+        $password = $hasher->compute('password');
+        $this->assertTrue(\is_string($password));
+        $this->assertTrue(!$hashe2->needsRehash($password));
+    }
+
+    /**
+     * @return void Returns nothing.
+     */
+    public function testArgon2i2BcryptRehash(): void
+    {
+        $hasher = new PasswordHasher(\PASSWORD_ARGON2I);
+        $this->assertEquals(\PASSWORD_ARGON2I, $hasher->passwordAlgo);
+        $password = $hasher->compute('password');
+        $this->assertTrue(\is_string($password));
+        $hasher2 = new PasswordHasher(\PASSWORD_BCRYPT);
+        $this->assertEquals(\PASSWORD_BCRYPT, $hashe2->passwordAlgo);
+        $this->assertTrue($hashe2->needsRehash($password));
+        $password = $hasher->compute('password');
+        $this->assertTrue(\is_string($password));
+        $this->assertTrue(!$hashe2->needsRehash($password));
+    }
+
+    /**
+     * @return void Returns nothing.
+     */
+    public function testArgon2i2Argon2idRehash(): void
+    {
+        $hasher = new PasswordHasher(\PASSWORD_ARGON2I);
+        $this->assertEquals(\PASSWORD_ARGON2I, $hasher->passwordAlgo);
+        $password = $hasher->compute('password');
+        $this->assertTrue(\is_string($password));
+        $hasher2 = new PasswordHasher(\PASSWORD_ARGON2ID);
+        $this->assertEquals(\PASSWORD_ARGON2ID, $hashe2->passwordAlgo);
+        $this->assertTrue($hashe2->needsRehash($password));
+        $password = $hasher->compute('password');
+        $this->assertTrue(\is_string($password));
+        $this->assertTrue(!$hashe2->needsRehash($password));
+    }
+
+    /**
+     * @return void Returns nothing.
+     */
+    public function testArgon2id2BcryptRehash(): void
+    {
+        $hasher = new PasswordHasher(\PASSWORD_ARGON2ID);
+        $this->assertEquals(\PASSWORD_ARGON2ID, $hasher->passwordAlgo);
+        $password = $hasher->compute('password');
+        $this->assertTrue(\is_string($password));
+        $hasher2 = new PasswordHasher(\PASSWORD_BCRYPT);
+        $this->assertEquals(\PASSWORD_BCRYPT, $hashe2->passwordAlgo);
+        $this->assertTrue($hashe2->needsRehash($password));
+        $password = $hasher->compute('password');
+        $this->assertTrue(\is_string($password));
+        $this->assertTrue(!$hashe2->needsRehash($password));
+    }
+
+    /**
+     * @return void Returns nothing.
+     */
+    public function testArgon2id2Argon2iRehash(): void
+    {
+        $hasher = new PasswordHasher(\PASSWORD_ARGON2ID);
+        $this->assertEquals(\PASSWORD_ARGON2ID, $hasher->passwordAlgo);
+        $password = $hasher->compute('password');
+        $this->assertTrue(\is_string($password));
+        $hasher2 = new PasswordHasher(\PASSWORD_ARGON2I);
+        $this->assertEquals(\PASSWORD_ARGON2I, $hashe2->passwordAlgo);
+        $this->assertTrue($hashe2->needsRehash($password));
+        $password = $hasher->compute('password');
+        $this->assertTrue(\is_string($password));
+        $this->assertTrue(!$hashe2->needsRehash($password));
+    }
 }
