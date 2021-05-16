f7397661357b8e17d312586edb4e73ae270bfcbf
lovephpforever <83395798+lovephpforever@users.noreply.github.com>
2021-05-16 16:03:38 -0400

    Fix syntax error.

--- src/Core/PasswordProtector.php
+++ src/Core/PasswordProtector.php
@@ -30,7 +30,7 @@ namespace LovePHPForever\Core;
  */
 interface PasswordProtector
 {
-    public function __construct(public int|string $passwordAlgo, array $options = []);
+    public function __construct(int|string $passwordAlgo, array $options = []);
     public function compute(string $password): ?string;
     public function verify(string $password, string $hash): bool;
     public function needsRehash(string $hash): bool;
