43171421568cbe117243abde4a8a8fe57f936f11
lovephpforever <83395798+lovephpforever@users.noreply.github.com>
2021-05-20 02:40:41 -0400

    Add some throttling logic.

--- src/Core/Security.php
+++ src/Core/Security.php
@@ -27,6 +27,7 @@ namespace LovePHPForever\Core;
 
 use RuntimeException;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\RateLimiter\LimiterInterface;
 
 /**
  * The password hasher.
@@ -36,14 +37,50 @@ final class Security implements Protector
     /**
      * Construct a new password hasher.
      *
-     * @param \Symfony\Component\HttpFoundation\Session\SessionInterface $session The session handler.
+     * @param \Symfony\Component\HttpFoundation\Session\SessionInterface $session       The session handler.
+     * @param \LovePHPForever\Core\CsrfProtector                         $csrfProtector The csrf protector.
+     * @param \Symfony\Component\RateLimiter\LimiterInterface            $rateLimiter   The symfony rate limiter.
      *
      * @return void Returns nothing.
      */
     public function __construct(
-        public SessionInterface $session
+        public SessionInterface $session,
+        public CsrfProtector $csrfProtector,
+        public LimiterInterface $rateLimiter
     ) {
         //
+    }
+
+    /**
+     * Check to see if too many requests have been made to the server.
+     *
+     * @return void Returns nothing.
+     */
+    public function hasTooManyServerRequestsAndConsume(): bool
+    {
+        if (headers_sent()) {
+            throw new RuntimeException('Headers already sent.');
+        }
+        $limit = $this->limiter->consume();
+        \header('X-RateLimit-Remaining: ' . (string) $limit->getRemainingTokens());
+        \header('X-RateLimit-Retry-After: ' . (string) $limit->getRetryAfter()->getTimestamp());
+        \header('X-RateLimit-Limit: ' . (string) $limit->getLimit());
+        return \false === $limit->isAccepted();
+    }
+
+    /**
+     * Code to run before processing a request.
+     *
+     * @param array $postData The post data array.
+     *
+     * @return void Returns nothing.
+     */
+    public function requestPreRun(array $postData): void
+    {
+        $this->csrfProtector->verify($postData);
+        if ($this->hasTooManyServerRequestsAndConsume()) {
+            throw new RuntimeException('Too many requests have been made to the server.');
+        }
     }
 
     /**
@@ -53,16 +90,16 @@ final class Security implements Protector
      *
      * @return void Returns nothing.
      */
-    public function applySecurityHeaders(bool $onlyApplyToLoggedInUsers = true): void
+    public function applySecurityHeaders(bool $onlyApplyToLoggedInUsers = \true): void
     {
         if (headers_sent()) {
             throw new RuntimeException('Headers already sent.');
-        } elseif ((bool) $this->session->get('logged_in', false) || !$onlyApplyToLoggedInUsers) {
+        } elseif ((bool) $this->session->get('logged_in', \false) || !$onlyApplyToLoggedInUsers) {
 			\header('X-Frame-Options: sameorigin');
 			\header('X-Content-Type-Options: nosniff');
-			\header('Cache-Control: no-store, no-cache, must-revalidate', true);
-			\header('Expires: Thu, 19 Nov 1981 00:00:00 GMT', true);
-			\header('Pragma: no-cache', true);
+			\header('Cache-Control: no-store, no-cache, must-revalidate', \true);
+			\header('Expires: Thu, 19 Nov 1981 00:00:00 GMT', \true);
+			\header('Pragma: no-cache', \true);
 		}
     }
 
@@ -81,10 +118,10 @@ final class Security implements Protector
     public function enforceSessionSecurityConfig(bool $cookieSameSite = "Lax", bool $enforceHttps = true): void
     {
         \ini_set('session.cookie_secure', $enforceHttps);
-        \ini_set('session.cookie_httponly', true);
+        \ini_set('session.cookie_httponly', \true);
         \ini_set('session.cookie_samesite', $cookieSameSite);
-        \ini_set('session.use_cookies', true);
-        \ini_set('session.use_only_cookies', true);
-        \ini_set('session.use_trans_sid', false);
+        \ini_set('session.use_cookies', \true);
+        \ini_set('session.use_only_cookies', \true);
+        \ini_set('session.use_trans_sid', \false);
     }
 }
